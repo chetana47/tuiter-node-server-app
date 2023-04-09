531ccac22c153142e825f7f33215c2e1af55d987
chetana47 <chetanareddy64@gmail.com>
2023-04-09 15:55:48 -0400

    a9 changes

--- app.js
+++ app.js
@@ -5,11 +5,15 @@ import UserController
     from "./controllers/users/users-controller.js"
 import TuitsController
     from "./controllers/tuits/tuits-controller.js";
+import mongoose from "mongoose";
 import cors from 'cors';
 const app = express()
 app.use(cors())
 app.use(express.json());
 TuitsController(app);
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
+
 app.get('/hello', (req, res) => {res.send('Hello daa!')})
 app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
 HelloController(app)
